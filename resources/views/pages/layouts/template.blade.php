2136db45363762d6296b22fd1a8b052eed9df1be
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-05-30 08:59:32 +0700

    menambahkan next previous button

--- resources/views/pages/layouts/template.blade.php
+++ resources/views/pages/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <title>Alat Musik Daerah</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
     <style>
         body {
             margin: 0;
